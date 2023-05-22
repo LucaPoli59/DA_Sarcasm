0be3091f9360810fc12782f1571a99215003df72
Luca Poli <poli.luca.marconi@gmail.com>
2023-05-22 16:30:55 +0200

    Fix per ottenere la path assoluta, indipendentemente dalla posizione di partenza

--- constants.py
+++ constants.py
@@ -1,5 +1,14 @@
 import os
 
+
+PROJECT_NAME = "data_analytics"
 PROJECT_PATH = os.getcwd()
+
+while os.path.basename(os.getcwd()) != PROJECT_NAME:
+    os.chdir("..")
+    PROJECT_PATH = os.getcwd()
+
+print(PROJECT_PATH)
+
 DATA_PATH = os.path.join(PROJECT_PATH, "dataset")
 GLOVE_PATH = os.path.join(DATA_PATH, "glove.json")
