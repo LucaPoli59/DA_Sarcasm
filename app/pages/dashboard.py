310a8ee2041686fbce2fedb3b21386176be4cbe9
Luca Poli <poli.luca.marconi@gmail.com>
2023-05-26 16:41:58 +0200

    Realizzazione boxplot delle frequenze, sistemazione titoli, e aggiustamenti grafico Info_Rate

--- app/pages/dashboard.py
+++ app/pages/dashboard.py
@@ -1,4 +1,5 @@
 from dash import html, dash_table, dcc, callback, Input, Output
+from plotly.subplots import make_subplots
 import plotly.express as px
 import dash_bootstrap_components as dbc
 import pandas as pd
@@ -19,7 +20,7 @@ len_dfs = {'text': pd.read_csv(os.path.join(constants.DATA_SP_PATH, "len_text.cs
 
 for df_name, df in len_dfs.items():
     df = df.rename(columns={df.columns[0]: 'len'})
-    df['prop_s'] = df['prop'] - target_info_rate
+    df['info_rate'] = abs(df['prop'] - target_info_rate) * 100
     df['prop'] = round(df['prop'] * 100, 0)
     len_dfs[df_name] = df.loc[abs(zscore(df['len']) < 3)]
 
@@ -30,6 +31,8 @@ for df_name, df in len_dfs.items():
     len_range[df_name].index = (len_range[df_name].index.values * 100).round().astype(int)
     tot_range[df_name] = df['tot'].quantile(np.arange(0, 1.01, 0.01))[::-1]
     tot_range[df_name].index = ((1 - tot_range[df_name].index.values) * 100).round().astype(int)
+
+len_frequency = {df_name: df['len'].repeat(df['tot']) for df_name, df in len_dfs.items()}
 
 layout = dbc.Container(className="fluid", children=[
     html.Center(html.H1("Dashboard", className="display-3 my-4")),
@@ -65,14 +68,19 @@ layout = dbc.Container(className="fluid", children=[
     dbc.Row(dash_table.DataTable(data=df_t.to_dict('records'), page_size=3,
                                  style_data={'whiteSpace': 'normal', 'height': 'auto'})),
     html.Hr(className="my-5"),
-    dbc.Container(className="d-flex flex-column justify-content-center align-items-center", children=[
-        html.Center(html.H3(id='len_title', children="Distribuzione della lunghezza del testo")),
+    dbc.Container(className="d-flex flex-column justify-content-center align-items-center mb-3", children=[
+        html.Center(html.H3(id='len_title', children="Analisi della lunghezza del testo")),
         dbc.RadioItems(id="feature_len_selector", options={'text': 'Testo', 'parent': 'Parent'}, value='text',
                        inline=True, className="date-group-items justify-content-center mt-3"),
     ]),
-    dcc.Graph(id="len_graph"),
+    html.Center(html.H5("Distribuzione lunghezza sentenze rispetto al numero di campioni")),
+    dcc.Graph(id="len_tot_graph", className="mb-3"),
+
+    html.Center(html.H5("Distribuzione Rateo informativo rispetto alla lunghezza delle sentenze")),
+    dcc.Graph(id="info_rate_graph"),
+
     dbc.Container(children=[
-        dbc.Label("Lunghezza delle sentenze:"),
+        dbc.Label("Numero di parole nelle sentenze:"),
         dcc.RangeSlider(id="len_slider", min=0, max=100, step=1, dots=False, value=(0, 50), className="mt-1",
                         allowCross=False),
     ]),
@@ -89,9 +97,10 @@ layout = dbc.Container(className="fluid", children=[
            Output(component_id='len_slider', component_property='value'),
            Output(component_id='tot_slider', component_property='marks'),
            Output(component_id='tot_slider', component_property='value'),
-           Output(component_id='len_title', component_property='children')],
+           Output(component_id='len_title', component_property='children'),
+           Output(component_id='len_tot_graph', component_property='figure')],
           [Input(component_id='feature_len_selector', component_property='value')])
-def update_sliders(ft_s):
+def update_sliders_len_tot_graph(ft_s):
     len_slider_marks = {mark: str(round(v)) for mark, v in len_range[ft_s].iloc[::20].items()}
     tot_slider_marks = {mark: str(round(v)) for mark, v in tot_range[ft_s].iloc[::10].items()}
 
@@ -101,19 +110,21 @@ def update_sliders(ft_s):
     else:
         title += "parent"
 
-    return len_slider_marks, (0, 50), tot_slider_marks, (0, 50), title
+    len_tot_graph = px.box(len_frequency[ft_s], x='len', labels={'len': 'Numero di parole nelle sentenze'})
+
+    return len_slider_marks, (0, 50), tot_slider_marks, (0, 50), title, len_tot_graph
 
 
-@callback(Output(component_id='len_graph', component_property='figure'),
+@callback(Output(component_id='info_rate_graph', component_property='figure'),
           [Input(component_id='feature_len_selector', component_property='value'),
            Input(component_id='len_slider', component_property='value'),
            Input(component_id='tot_slider', component_property='value')])
-def update_len_graph(ft_s, len_value, tot_value):
+def update_info_rate_graph(ft_s, len_value, tot_value):
     df_len = len_dfs[ft_s]
     len_value = len_range[ft_s][len_value[0]], len_range[ft_s][len_value[1]]
     tot_value = (tot_range[ft_s][tot_value[0]], tot_range[ft_s][tot_value[1]])[::-1]
 
-    return px.bar(df_len.loc[df_len['len'].between(*len_value) & df_len['tot'].between(*tot_value)],
-                  x="len", y="prop_s", text_auto=True, hover_data=['prop', 'tot'], range_y=[-0.51, 0.51],
-                  labels={'len': 'Numero parole', 'prop': 'Sarcastica (%)',
-                          'prop_s': 'Rateo informativo', 'tot': 'Numero campioni'})
+    return px.bar(df_len.loc[df_len['tot'].between(*tot_value) & df_len['len'].between(*len_value)],
+                  x="len", y="info_rate", text_auto=True, hover_data=['prop', 'tot'], range_y=[0, 50],
+                  labels={'len': 'Numero di parole nelle sentenze', 'prop': 'Sarcastica (%)',
+                          'info_rate': 'Rateo informativo', 'tot': 'Numero campioni'})
