5effcb56dacadbd65b4624332bb37cf2ea02634f
Luca Poli <poli.luca.marconi@gmail.com>
2023-05-29 19:00:43 +0200

    Aggiunte ag grid per il confronto di tutti i testi e per l'analisi dell'info rate, aggiunto grafo info rate medio

--- app/pages/text.py
+++ app/pages/text.py
@@ -10,6 +10,7 @@ import timeit
 from wordcloud import WordCloud
 import json
 import constants
+from dash_ag_grid import AgGrid
 
 
 def get_wc_fig(wc, info_rate):
@@ -22,15 +23,30 @@ def get_wc_fig(wc, info_rate):
 
 start = timeit.default_timer()
 
-selector_options = {'tokenized': 'Normale', 'nsw': 'No Stop Words', 'st': 'Stemming',
-                    'nsw_st': 'No Stop Words e Stemming', 'punctuation': 'Punteggiatura'}
+selector_options = {'tokenized': 'Normale', 'punctuation': 'Punteggiatura', 'nsw': 'Senza Stopwords', 'st': 'Stemming',
+                    'nsw_st': 'Senza Stopwords e Stemming'}
 
 dfs_sp = {name: pd.read_csv(os.path.join(constants.DATA_SP_PATH, "text_" + name + ".csv"), index_col="element")
           for name in selector_options.keys()}
+dfs_info_avg = pd.Series(index=selector_options.keys(), name='info_rate_avg', dtype=float)
+df_texts = pd.read_csv(os.path.join(constants.DATA_OUT_PATH, "train_text.csv"), index_col="index")
 
 for name in dfs_sp.keys():
-    dfs_sp[name]['tot_s'] = round(dfs_sp[name]['tot'] / dfs_sp[name]['tot'].sum() * 100, 2)
+    dfs_sp[name]['tot_s'] = dfs_sp[name]['tot'] / dfs_sp[name]['tot'].sum() * 100
     dfs_sp[name]['prop'] = round(dfs_sp[name]['prop'] * 100)
+    dfs_info_avg[name] = np.average(dfs_sp[name]['info_rate'].values, weights=dfs_sp[name]['tot_s'].values)
+
+dfs_info_avg = dfs_info_avg.drop('punctuation')
+
+sp_cols_to_grid = {'element': 'Testo', 'tot_s': 'Frequenza %', 'tot': 'Frequenza', 'prop': 'Proporzione sarcastica %',
+                   'info_rate': 'Rateo informativo'}
+sp_grids = {name: AgGrid(
+    rowData=dfs_sp[name].reset_index()[list(sp_cols_to_grid.keys())].to_dict('records'),
+    columnDefs=[{'field': col, 'headerName': sp_cols_to_grid[col]} for col, col_name in sp_cols_to_grid.items()],
+    defaultColDef={"resizable": True, "sortable": True, "filter": True, "minWidth": 125,
+                   "wrapText": True, 'autoHeight': True},
+    columnSize='sizeToFit',
+) for name in dfs_sp.keys()}
 
 info_rate_graphs_dict = {}
 for name in selector_options.keys():
@@ -64,22 +80,22 @@ end = timeit.default_timer()
 print("Tempo di caricamento: ", end - start)
 
 
-@callback([Output("text_info_rate_graphs_wc", "figure"), Output("text_info_rate_graphs_bar", "figure"),
-           Output("text_info_rate_graph_wc", "figure"), Output("text_info_rate_graph_bar", "figure")],
+@callback([Output("text_info_rate_graph_wc", "figure"), Output("text_info_rate_graph_bar", "figure"),
+           Output("text_info_rate_graphs_wc", "figure"), Output("text_info_rate_graphs_bar", "figure")],
           [Input("text_info_rate_text_selector", "value")],
           prevent_initial_call=True)
 def update_info_rate_graphs(text_selector):
     graphs = info_rate_graphs_dict[text_selector]
 
-    return go.Figure(data=graphs['wc']['img'], layout=wc_layout), graphs['bar'], \
-           go.Figure(data=list(graphs['wc'].values()), layout=wc_layout), graphs['bar']
+    return go.Figure(data=list(graphs['wc'].values()), layout=wc_layout), \
+           go.Figure(data=graphs['wc']['img'], layout=wc_layout), *[graphs['bar']] * 2
 
 
 @callback([Output("text_info_graph_tab1", "label_class_name"), Output("text_info_graph_tab2", "label_class_name"),
            Output("text_info_graph_tab3", "label_class_name")], [Input("text_info_graph_selector", "active_tab")])
 def update_tabs_active_style(graph_selector):
-    return ["text-primary fs-5" if graph_selector == 'wc_bar' else "text-dark fs-5",
-            "text-primary fs-5" if graph_selector == 'wc' else "text-dark fs-5",
+    return ["text-primary fs-5" if graph_selector == 'wc' else "text-dark fs-5",
+            "text-primary fs-5" if graph_selector == 'wc_bar' else "text-dark fs-5",
             "text-primary fs-5" if graph_selector == 'bar' else "text-dark fs-5"]
 
 
@@ -90,14 +106,34 @@ def update_info_rate_stats(text_selector):
     return graphs['hist'], graphs['box']
 
 
+@callback([Output("text_info_rate_grid", "children")], [Input("text_info_rate_text_selector", "value")],
+          prevent_initial_call=True)
+def update_sp_grid(text_selector):
+    return [sp_grids[text_selector]]
+
+
 def_graph = update_info_rate_graphs('tokenized')
 
 layout = dbc.Container(className="fluid", children=[
     html.Center(html.H1("Text Analysis", className="display-3 my-4")),
 
+    html.Center(html.H3(className='my-3', children="Testi a confronto")),
+    html.Div(AgGrid(
+        rowData=df_texts.to_dict('records'),
+        columnDefs=[{'field': col, 'headerName': selector_options[col]} for col in df_texts.columns],
+        defaultColDef={"resizable": True, "sortable": True, "filter": True, "minWidth": 125,
+                       "wrapText": True, 'autoHeight': True},
+        columnSize='sizeToFit',
+    )),
+
+    html.Hr(className="my-5"),
+    html.Center(html.H3("Rateo informativo medio")),
+    dcc.Graph(figure=px.bar(dfs_info_avg.rename(selector_options).reset_index(), x='index', y='info_rate_avg',
+                            labels={'index': 'Tipo di testo', 'info_rate_avg': 'Rateo informativo medio'})),
+    html.Hr(className="my-5"),
     dbc.Container(className="border border-secondary", children=[
         dbc.Container(className="d-flex flex-column justify-content-center align-items-center mt-3", children=[
-            html.Center(html.H3("Analisi del rateo informativo degli elementi del testo")),
+            html.Center(html.H2("Analisi del rateo informativo degli elementi di ogni tipo di testo")),
             dbc.RadioItems(id="text_info_rate_text_selector", inline=True,
                            options=selector_options, value='tokenized',
                            className="date-group-items justify-content-center my-2"),
@@ -112,28 +148,30 @@ layout = dbc.Container(className="fluid", children=[
         html.Hr(),
         dbc.Tabs(className="nav nav-tabs nav-justified justify-content-center align-items-center", active_tab='wc_bar',
                  id="text_info_graph_selector", children=[
-                dbc.Tab(label='Entrambi', tab_id='wc_bar', label_class_name="text-dark fs-5", id="text_info_graph_tab1",
+                dbc.Tab(label='Word Cloud', tab_id='wc', label_class_name="text-dark fs-5", id="text_info_graph_tab1",
+                        children=dcc.Loading(type="circle",
+                                             children=dcc.Graph(figure=def_graph[0], id='text_info_rate_graph_wc'))
+                        ),
+
+                dbc.Tab(label='Entrambi', tab_id='wc_bar', label_class_name="text-dark fs-5", id="text_info_graph_tab2",
                         children=[
                             dcc.Loading(type="circle", children=[
                                 dbc.Row([
-                                    dbc.Col(dcc.Graph(figure=def_graph[0], id='text_info_rate_graphs_wc'),
+                                    dbc.Col(dcc.Graph(figure=def_graph[1], id='text_info_rate_graphs_wc'),
                                             className="col-sm-6"),
-                                    dbc.Col(dcc.Graph(figure=def_graph[1], id='text_info_rate_graphs_bar'),
+                                    dbc.Col(dcc.Graph(figure=def_graph[2], id='text_info_rate_graphs_bar'),
                                             className="col-sm-6")
                                 ])
                             ])
                         ]),
-                dbc.Tab(label='Word Cloud', tab_id='wc', label_class_name="text-dark fs-5", id="text_info_graph_tab2",
-                        children=dcc.Loading(type="circle",
-                                             children=dcc.Graph(figure=def_graph[2], id='text_info_rate_graph_wc'))
-                        ),
                 dbc.Tab(label='Grafo a barre', tab_id='bar', label_class_name="text-dark fs-5",
                         id="text_info_graph_tab3",
                         children=dcc.Loading(type="circle",
                                              children=dcc.Graph(figure=def_graph[3], id='text_info_rate_graph_bar'))
                         ),
             ]),
-
+        html.Hr(className="my-5"),
+        dcc.Loading(type="circle", children=html.Div(children=[sp_grids['tokenized']], id="text_info_rate_grid")),
     ]),
 
 ])
