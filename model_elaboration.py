0a0d17f0d3d5d7dd5d1780f373d3fb8a3da09600
Luca Poli <poli.luca.marconi@gmail.com>
2023-06-05 12:37:34 +0200

    Versione parziale di elaborazione del modello, con text e parent uniti. versione con transformer dopo bert

--- model_elaboration.py
+++ model_elaboration.py
@@ -26,40 +26,25 @@ df_val['text_len'] = df_val['text_len'] / df_val['text_len'].max()
 df_val['parent_len'] = df_val['parent'].apply(lambda x: len(x.split()))
 df_val['parent_len'] = df_val['parent_len'] / df_val['parent_len'].max()
 
-
-text_parent_backbone = keras_nlp.models.BertBackbone.from_preset("bert_tiny_en_uncased")
-text_parent_processor = keras_nlp.models.BertPreprocessor.from_preset("bert_tiny_en_uncased")
-text_parent_backbone.trainable = False
-
+bert_backbone = keras_nlp.models.BertBackbone.from_preset("bert_tiny_en_uncased")
+bert_processor = keras_nlp.models.BertPreprocessor.from_preset("bert_tiny_en_uncased")
+bert_backbone.trainable = False
 
 text_input = tf.keras.Input(shape=(), dtype=tf.string, name='text')
 parent_input = tf.keras.Input(shape=(), dtype=tf.string, name='parent')
 
-text_parent_layers = text_parent_processor([text_input, parent_input])
-print(text_parent_layers)
-text_parent_layers = text_parent_backbone(text_parent_layers)['pooled_output']
-
-
-# text_layers = text_preprocessor(text_input)
-# text_layers = text_backbone(text_layers)['sequence_output']
-# parent_layers = parent_preprocessor(parent_input)
-# parent_layers = parent_backbone(parent_layers)['sequence_output']
-#
-# text_layers = keras_nlp.layers.TransformerEncoder(num_heads=2, intermediate_dim=521, name="text.encoder"
-#                                                   )(text_layers)[:, text_backbone.cls_token_index, :]
-# parent_layers = keras_nlp.layers.TransformerEncoder(num_heads=2, intermediate_dim=521, name="parent.encoder"
-#                                                     )(parent_layers)[:, parent_backbone.cls_token_index, :]
-#
-# text_layers = tf.keras.layers.Dense(10, name="text.encoded.pr")(text_layers)
+text_parent_layers = bert_processor([text_input, parent_input])
+text_parent_layers = bert_backbone(text_parent_layers)['sequence_output']
+text_parent_layers = keras_nlp.layers.TransformerEncoder(num_heads=2, intermediate_dim=521, name="text_parent.encoder"
+                                                         )(text_parent_layers)[:, bert_backbone.cls_token_index, :]
+text_parent_layers = tf.keras.layers.Dense(10, name="text_parent.encoded.pr")(text_parent_layers)
 # parent_layers = tf.keras.layers.Dense(10, name="parent.encoded.pr")(parent_layers)
 # text_parent_layers = tf.keras.layers.Concatenate(name="text_parent.concat")([text_layers, parent_layers,
 #                                                                              text_len_input, parent_len_input])
 # text_parent_layers = tf.keras.layers.Dense(10, activation=activations.relu, name="text_parent.pr")(text_parent_layers)
 
-
-
-global_layers = tf.keras.layers.Dense(10, name="global.pr", activation=activations.relu)(text_parent_layers)
-output = tf.keras.layers.Dense(1, activation=activations.sigmoid, name='output')(global_layers)
+# global_layers = tf.keras.layers.Dense(10, name="global.pr", activation=activations.relu)(text_parent_layers)
+output = tf.keras.layers.Dense(1, activation=activations.sigmoid, name='output')(text_parent_layers)
 
 model = keras.Model([text_input, parent_input], output)
 model.compile(optimizer="adam", loss=losses.BinaryCrossentropy(), metrics=[metrics.BinaryAccuracy()])
